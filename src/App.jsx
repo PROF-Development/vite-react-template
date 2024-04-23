017f0fb89991366d4ef89960c1efef4f45dccf82
GlebVas7 <Gleb79654588110@yandex.ru>
2024-04-23 14:49:42 +0300

    PROF-1 Pre-commit хуки

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 /*import React from 'react';*/
 import { RouterProvider } from 'react-router-dom';
+
 import { router } from './utils/navigation/router';
 
 function App() {
-  return <RouterProvider router={router} />;
+    return <RouterProvider router={router} />;
 }
 
 export default App;
