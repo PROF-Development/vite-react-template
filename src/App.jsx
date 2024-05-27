6281ba14718114ce4bc21ca7e76dc6a348a410de
vadim-221212 <vdmed@bk.ru>
2024-05-27 21:35:51 +0300

    PROF-2 App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { RouterProvider } from "react-router-dom"
 import {router} from './utils/navigation/router'
-import config from './config';
 
-console.log(config);
 
 function App() {
     return <RouterProvider router={router}/>
 }
 
-export default App
+export default App;
